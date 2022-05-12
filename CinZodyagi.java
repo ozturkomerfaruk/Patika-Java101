8f7fe95cf3737898706f11059914f10f96c79c6b
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2022-05-12 13:03:02 +0300

    cinZodyagi

---
+++ CinZodyagi.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dogum yilinizi giriniz: ");
+        int year = input.nextInt();
+        int kalan = year % 12;
+        switch (kalan) {
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Kopek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Okuz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavsan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yilan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+
+    }
+}
