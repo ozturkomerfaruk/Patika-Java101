57e04f8bf1f46e583bdd939412612249b1f45da5
ozturkomerfaruk <56068905+ozturkomerfaruk@users.noreply.github.com>
2022-05-14 17:03:33 +0300

    Java Loops I

---
+++ Hackerrank - Java Loops I.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+    }
+}
